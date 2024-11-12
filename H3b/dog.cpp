eefbdf0993026f199b97554814861096f47ed1d0
EnniKelloniemi <enni.kelloniemi@gmail.com>
2024-11-12 18:38:51 +0200

    h3b

---
+++ H3b/dog.cpp
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+Dog::Dog() {
+
+}
+
+void Dog::callOut()
+{
+    cout<<"Koita haukkuu"<<endl;
+}
+
+Dog::~Dog()
+{
+    cout<<"Koira olio tuhoutui"<<endl;
+}
---
+++ H3b/main.cpp
@@ -0,0 +1,15 @@
+#include "animal.h"
+#include "dog.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    Animal objAnimal;
+    Dog objDog;
+    objAnimal.callOut();
+    objDog.callOut();
+    return 0;
+}
